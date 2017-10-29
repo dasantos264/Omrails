5f440cd29414d3d65e8a0a393f8d406b1e310588
Diana Santos <dasantos264@live.com>
2017-10-29 16:20:28 -0400

    Added homepage and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get'about' =>'pages#about'
+
+  get'contact' =>'pages#contact'
+
+
+  get 'page/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
